fd5a02232ed5e60aedc93027989aaa87e457b6b1
zlodiak <prozaik81-2@yandex.ru>
2016-05-13 15:36:07 +0300

    informer fix

--- js/project/views/widgetsViews.js
+++ js/project/views/widgetsViews.js
@@ -1,16 +1,8 @@
 APP.InformerView = Backbone.View.extend({  
 
-  initialize: function() {     console.log('ini', this.model.get('rockets'))
-    this.listenTo(this.model, 'change', this.initialize);
-
-    this.params = {
-      score: this.model.get('score'),
-      rockets: this.model.get('rockets'),
-      energy: this.model.get('energy'),
-      speed: this.model.get('speed'),
-    };
-
-    this.render();
+  initialize: function() {   
+    this.listenTo(this.model, 'change', this.refreshOutputData); 
+    this.refreshOutputData();   
   },
 
   template: _.template($('#informerTpl').html()),
@@ -18,6 +10,20 @@ APP.InformerView = Backbone.View.extend({
   render: function() {    
     this.$el.html(this.template(this.params));      
     return this;
+  },
+
+  refreshOutputData: function() {  
+    this.getParams();
+    this.render();
+  },
+
+  getParams: function() {    
+    this.params = {
+      score: this.model.get('score'),
+      rockets: this.model.get('rockets'),
+      energy: this.model.get('energy'),
+      speed: this.model.get('speed'),
+    };
   }
 
 });
