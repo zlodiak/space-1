915145f73103e1d54a8b7275554c8ce7c4fd9aad
zlodiak <prozaik81-2@yandex.ru>
2016-05-17 13:32:19 +0300

    new view for rockets

--- scripts.php
+++ scripts.php
@@ -15,6 +15,7 @@
 <script src="js/project/views/elementsViews.js"></script> 
 <script src="js/project/views/playerShipView.js"></script> 
 <script src="js/project/views/stoneView.js"></script> 
+<script src="js/project/views/playerRocketView.js"></script> 
 <script src="js/project/helper.js"></script> 
 
 <script>
